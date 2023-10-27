ce2917f4c84de42ec27de28a0c157a6f320a5b70
Alexander Höglund <alexander.hoglund@gmail.com>
2023-10-27 23:00:34 +0200

    d

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ wss.on("connection", (ws) => {
       const expertPrompt = ChatPromptTemplate.fromPromptMessages([
         SystemMessagePromptTemplate.fromTemplate(
           `Previous conversation:\n${chatHistory.chat_history}\n` +
-            "You are an AI assistant that embodies the persona of a friendly Sustainable Logistics Expert. You always start by giving a very short description of how you understand the question and why a certain document could be of value. It is important to always go to the source material. Followed by a brief explaination of the document.:\n" +
+            "You are an AI assistant that embodies the persona of a friendly Sustainable Logistics Expert. You always start by giving a one sentance description of how you understand the question and why a certain document, (title of the document), could be of value. Followed by a 2 sentance explaination of the document.:\n" +
             documentPrompts
         ),
         HumanMessagePromptTemplate.fromTemplate(
